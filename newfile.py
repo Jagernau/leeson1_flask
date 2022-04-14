801405fae046e6083958e606f859644e18dc7594
jagernau <stepanenkov25@gmail.com>
2022-04-14 13:30:45 +0300

    new6

--- newfile.py
+++ newfile.py
@@ -8,10 +8,35 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    return "Главная страничка"
+    show = ""
+    for i in names:
+        show +=  f"\nИмя - {i['name']}\nПозиция - {i['position']}\nНавыки - {i['skills']}\n\n"
+    return f"<pre >{show}<pre>"
+    
    
-@app.route("/prof/<uid>")
-def page_prof(uid):
-    return f"Профиль {uid}"
+@app.route("/candidates/<x>")
+def page_candidates(x):
+    x = int(x)
+    pic = ""
+    info = ""
+    for i in names:
+        if x == i["id"]:
+            pic += i["picture"]
+            info += f"\nИмя - {i['name']}\nПозиция - {i['position']}\nНавыки - {i['skills']}\n\n"
+            
+    return f"<img src = {pic}>\n<pre >{info}<pre>"
+            
+
+@app.route("/skills/<y>")
+def page_skills(y):
+    y = str(y)
+    by_skill = ""
+    for i in names:
+        if y in i["skills"]:
+            by_skill += f"\nИмя - {i['name']}\nПозиция - {i['position']}\nНавыки - {i['skills']}\n\n"
+        if y.title() in i["skills"]:
+            by_skill += f"\nИмя - {i['name']}\nПозиция - {i['position']}\nНавыки - {i['skills']}\n\n"
+        
+    return f"<pre >{by_skill}<pre>"
 
 app.run()
